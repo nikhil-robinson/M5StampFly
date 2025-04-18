665c6b07d98b188b39654eb68b2024daa5185af9
Nikhil Robinson <nikhil@bibox.in>
2025-04-18 14:25:26 +0530

    feat: clean up main.cpp by removing unused sensor includes and commented code

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,11 @@
 #include <Arduino.h>
 #include <FastLED.h>
 #include "flight_control.hpp"
-#include "sensor.hpp"
-#include "Bitcraze_PMW3901.h"
-#include "optical_flow.hpp"
-#include "tof.hpp"
-#include "imu.hpp"
-#include "button.hpp"
-#include "flowdeck_v1v2.hpp"
 
 // VL53L0X_ADDRESS           0x29
 // MPU6886_ADDRESS           0x68
 // BMP280_ADDRESS            0x76
 
-
-#if 0
 void setup() {
     init_copter();
     delay(100);
@@ -23,46 +14,3 @@ void setup() {
 void loop() {
     loop_400Hz();
 }
-
-
-#else
-
-// Bitcraze_PMW3901 flow(12);
-
-
-void setup() {
-    // delay(1000 * 10);
-    // init_copter();
-    init_button();
-    USBSerial.begin(115200);
-    delay(1500);
-    USBSerial.printf("Start StampFly!\r\n");
-
-    sensor_init();
-    flowdeck2Init();
-
-    USBSerial.printf("Finish sensor init!\r\n");
-    delay(100);
-}
-
-void loop() 
-{
-    int16_t deltaX,deltaY;
-    // read_optical_flow(&deltaX,&deltaY);
-    imu_update();
-    float acc_x  = imu_get_acc_x();
-    float acc_y  = imu_get_acc_y();
-    float acc_z  = imu_get_acc_z();
-    float gyro_x = imu_get_gyro_x();
-    float gyro_y = imu_get_gyro_y();
-    float gyro_z = imu_get_gyro_z();
-
-    USBSerial.printf("X[%d] Y[%d]\r\n",deltaX,deltaY);
-    USBSerial.printf("%d mm\r\n",tof_bottom_get_range());
-    USBSerial.printf("ACC-X[%f] ACC-Y[%f] ACC-Z[%f]\r\n",acc_x,acc_y,acc_z);
-    USBSerial.printf("GYRO-X[%f] GYRO-Y[%f] GYRO-Z[%f]\r\n",gyro_x,gyro_y,gyro_z);
-    delay(100);
-
-}
-
-#endif
--- src/rc.cpp
+++ src/rc.cpp
@@ -24,68 +24,71 @@
  */
 
 #include "rc.hpp"
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
+#include <WiFi.h>
+#include <esp_now.h>
+#include <esp_wifi.h>
 #include "flight_control.hpp"
-#include "pid.hpp"
-#include "optical_flow.hpp"
-#include "tof.hpp"
 
 // esp_now_peer_info_t slave;
 
 volatile uint16_t Connect_flag = 0;
 
-// BLE Service and Characteristic UUIDs
-#define SERVICE_UUID        "12345678-1234-5678-1234-56789abcdef0"  // Custom Service UUID
-#define CHARACTERISTIC_UUID "abcd1234-5678-1234-5678-abcdef123456"  // Custom Characteristic UUID
-
-#define FRAME_BUFFER_LEN 25
-
-// Buffer to store received data (uint8_t array)
-uint8_t receivedData[100];      // Adjust the size if needed
-size_t receivedDataLength = 0;  // Track the length of received data
-
-uint8_t sentData[100];      // Adjust the size if needed
-size_t sentDataLength = 0;  // Track the length of received data
-
+// Telemetry相手のMAC ADDRESS 4C:75:25:AD:B6:6C
+// ATOM Lite (C): 4C:75:25:AE:27:FC
+// 4C:75:25:AD:8B:20
+// 4C:75:25:AF:4E:84
+// 4C:75:25:AD:8B:20
+// 4C:75:25:AD:8B:20 赤水玉テープ　ATOM lite
 uint8_t TelemAddr[6] = {0};
+// uint8_t TelemAddr[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 volatile uint8_t MyMacAddr[6];
 volatile uint8_t peer_command[4] = {0xaa, 0x55, 0x16, 0x88};
 volatile uint8_t Rc_err_flag     = 0;
+esp_now_peer_info_t peerInfo;
 
 // RC
 volatile float Stick[16];
 volatile uint8_t Recv_MAC[3];
 
-volatile uint8_t ble_send_status = 0;
+void on_esp_now_sent(const uint8_t *mac_addr, esp_now_send_status_t status);
 
 // 受信コールバック
-void OnDataRecv(const uint8_t *recv_data, int data_len) {
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *recv_data, int data_len) {
     Connect_flag = 0;
 
     uint8_t *d_int;
     // int16_t d_short;
     float d_float;
 
-    // Recv_MAC[0] = recv_data[0];
-    // Recv_MAC[1] = recv_data[1];
-    // Recv_MAC[2] = recv_data[2];
+    if (!TelemAddr[0] && !TelemAddr[1] && !TelemAddr[2] && !TelemAddr[3] && !TelemAddr[4] && !TelemAddr[5]) {
+        memcpy(TelemAddr, mac_addr, 6);
+        memcpy(peerInfo.peer_addr, TelemAddr, 6);
+        peerInfo.channel = CHANNEL;
+        peerInfo.encrypt = false;
+        if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+            USBSerial.println("Failed to add peer2");
+            memset(TelemAddr, 0, 6);
+        } else {
+            esp_now_register_send_cb(on_esp_now_sent);
+        }
+    }
 
-    // if ((recv_data[0] == MyMacAddr[3]) && (recv_data[1] == MyMacAddr[4]) && (recv_data[2] == MyMacAddr[5])) {
-    Rc_err_flag = 0;
-    // } else {
-    //     Rc_err_flag = 1;
-    //     return;
-    // }
+    Recv_MAC[0] = recv_data[0];
+    Recv_MAC[1] = recv_data[1];
+    Recv_MAC[2] = recv_data[2];
+
+    if ((recv_data[0] == MyMacAddr[3]) && (recv_data[1] == MyMacAddr[4]) && (recv_data[2] == MyMacAddr[5])) {
+        Rc_err_flag = 0;
+    } else {
+        Rc_err_flag = 1;
+        return;
+    }
 
     // checksum
     uint8_t check_sum = 0;
     for (uint8_t i = 0; i < 24; i++) check_sum = check_sum + recv_data[i];
     // if (check_sum!=recv_data[23])USBSerial.printf("checksum=%03d recv_sum=%03d\n\r", check_sum, recv_data[23]);
     if (check_sum != recv_data[24]) {
-        // USBSerial.printf("ERROR checksum=%03d recv_sum=%03d\n\r", check_sum, recv_data[23]);
         Rc_err_flag = 1;
         return;
     }
@@ -125,227 +128,84 @@ void OnDataRecv(const uint8_t *recv_data, int data_len) {
     Stick[LOG] = 0.0;
     // if (check_sum!=recv_data[23])USBSerial.printf("checksum=%03d recv_sum=%03d\n\r", check_sum, recv_data[23]);
 
-    USBSerial.printf("%ld %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %d %d\n\r", millis(), Stick[THROTTLE],
-                     Stick[AILERON], Stick[ELEVATOR], Stick[RUDDER], Stick[BUTTON_ARM], Stick[BUTTON_FLIP],
-                     Stick[CONTROLMODE], Stick[ALTCONTROLMODE], ahrs_reset_flag, Stick[LOG]);
+#if 0
+   USBSerial.printf("%6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f  %6.3f\n\r", 
+                                             Stick[THROTTLE],
+                                             Stick[AILERON],
+                                             Stick[ELEVATOR],
+                                             Stick[RUDDER],
+                                             Stick[BUTTON_ARM],
+                                             Stick[BUTTON_FLIP],
+                                             Stick[CONTROLMODE],
+                                             Stick[ALTCONTROLMODE],
+                                             Stick[LOG]);
+#endif
 }
 
-volatile bool data_task_running = false;
-
-void data_sender(void *pvParameters);
-void positionHold(void *pvParameters);
-
-class MyCallbacks : public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
-        uint8_t *value = pCharacteristic->getData();
-        size_t length  = pCharacteristic->getLength();
-
-        // Store the received data into the buffer
-        // if (length > 0) {
-        //     memcpy(receivedData + receivedDataLength, value, length);  // Copy data into receivedData buffer
-        //     receivedDataLength += length;
-        // }
-
-        // // USBSerial.printf("GOT BLE data of size %d\n\r",receivedDataLength);
-
-        // if (receivedDataLength == FRAME_BUFFER_LEN) {
-        //     OnDataRecv(receivedData, receivedDataLength);
-        //     // memset(receivedData,0,sizeof(receivedData));
-        //     receivedDataLength = 0;
-        // }
-
-        if ((value[0] == 'S') && (value[1] == 'T') && !data_task_running) {
-            xTaskCreatePinnedToCore(data_sender, "data_sender", 4096, NULL, 20, NULL, 0);
-        }
-    }
-
-    void onRead(BLECharacteristic *pCharacteristic) {
-        // Send the received data back to the client if available
-        if (receivedDataLength > 0) {
-            pCharacteristic->setValue(sentData, sentDataLength);  // Set the value to send back
-        }
-    }
-};
-
-BLECharacteristic *pCharacteristic = nullptr;
-bool deviceConnected               = false;
-
-class MyServerCallbacks : public BLEServerCallbacks {
-    void onConnect(BLEServer *pServer) {
-        deviceConnected = true;
-        delay(500);                   // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising();  // restart advertising
-    }
-
-    void onDisconnect(BLEServer *pServer) {
-        deviceConnected = false;
-    }
-};
-
-enum MovementState { MOVE_FORWARD, STOP_FORWARD, MOVE_BACKWARD, STOP_BACKWARD };
-
-MovementState moveState = MOVE_FORWARD;
-
-float focal_length = 35.0;
-
-float calculateDistanceCM(int16_t deltaX, int16_t deltaY, int16_t height_mm) {
-    float height_cm = height_mm / 10.0;  // Convert mm to cm
-
-    float dx          = (deltaX * height_cm) / focal_length;
-    float dy          = (deltaY * height_cm) / focal_length;
-    float distance_cm = sqrt(dx * dx + dy * dy);  // Total distance in cm
-
-    USBSerial.print("Height (mm): ");
-    USBSerial.print(height_mm);
-    USBSerial.print("\tDelta X (cm): ");
-    USBSerial.print(dx);
-    USBSerial.print("\tDelta Y (cm): ");
-    USBSerial.print(dy);
-    USBSerial.print("\tTotal Distance (cm): ");
-    USBSerial.println(distance_cm);
-
-    return distance_cm;
-}
-
-void positionHold(void *pvParameters) {
-    const float loopInterval = 0.0025f;  // 400Hz
-
-    USBSerial.printf("SET DEFAULT VALUE\r\n");
-    Stick[THROTTLE]       = 0.0;
-    Stick[AILERON]        = 0.0;
-    Stick[ELEVATOR]       = 0.0;
-    Stick[RUDDER]         = 0.0;
-    Stick[BUTTON_ARM]     = 0.0;
-    Stick[BUTTON_FLIP]    = 0.0;
-    Stick[CONTROLMODE]    = 0.0;
-    Stick[ALTCONTROLMODE] = 5.0;
-    ahrs_reset_flag       = 0.0;
-    Stick[LOG]            = 0.0;
-
-    vTaskDelay(pdMS_TO_TICKS(5000));
-
-    USBSerial.printf("AHRS RESET\r\n");
-
-    ahrs_reset_flag = 1.0;
-    vTaskDelay(pdMS_TO_TICKS(200));
-    ahrs_reset_flag = 0.0;
-
-    USBSerial.printf("ARM\r\n");
-    Stick[BUTTON_ARM] = 1.0;
-    vTaskDelay(pdMS_TO_TICKS(200));
-    Stick[BUTTON_ARM] = 0.0;
-    Stick[THROTTLE]       = -0.5;
-
-    USBSerial.printf("LIFT OFF\r\n");
-    vTaskDelay(pdMS_TO_TICKS(2000));
-    Stick[ELEVATOR] =0.2;
-    vTaskDelay(pdMS_TO_TICKS(5000));
-    Stick[ELEVATOR] =0.00;
-    vTaskDelay(pdMS_TO_TICKS(5000));
-    Stick[ELEVATOR] =-0.2;
-    vTaskDelay(pdMS_TO_TICKS(5000));
-    Stick[ELEVATOR] =0.00;
-    vTaskDelay(pdMS_TO_TICKS(5000));
-    Stick[THROTTLE] = -0.0;
-    vTaskDelay(pdMS_TO_TICKS(5000));
-    vTaskDelete(NULL);
-}
-
-void data_sender(void *pvParameters) {
-    data_task_running = true;
-    // vTaskDelay(pdMS_TO_TICKS(5000));
-    USBSerial.printf("SET DEFAULT VALUE\r\n");
-    Stick[THROTTLE]       = 0.0;
-    Stick[AILERON]        = 0.0;
-    Stick[ELEVATOR]       = 0.0;
-    Stick[RUDDER]         = 0.0;
-    Stick[BUTTON_ARM]     = 0.0;
-    Stick[BUTTON_FLIP]    = 0.0;
-    Stick[CONTROLMODE]    = 0.0;
-    Stick[ALTCONTROLMODE] = 4.0;
-    ahrs_reset_flag       = 0.0;
-    Stick[LOG]            = 0.0;
-
-    vTaskDelay(pdMS_TO_TICKS(5000));
-
-    USBSerial.printf("AHRS RESET\r\n");
-
-    ahrs_reset_flag = 1.0;
-    vTaskDelay(pdMS_TO_TICKS(200));
-    ahrs_reset_flag = 0.0;
-
-    USBSerial.printf("ARM\r\n");
-    Stick[BUTTON_ARM] = 1.0;
-    vTaskDelay(pdMS_TO_TICKS(200));
-    Stick[BUTTON_ARM] = 0.0;
-    USBSerial.printf("LIFT OFF\r\n");
-    vTaskDelay(pdMS_TO_TICKS(3000));
-    for (float i = 0.0; i > -0.1; i -= 0.001) {
-        Stick[ELEVATOR] = i;
-        USBSerial.printf("ELEVETOR %6.3f\r\n", Stick[ELEVATOR]);
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
-    Stick[ELEVATOR] = -0.1;
-    USBSerial.printf("ELEVETOR %6.3f\r\n", Stick[ELEVATOR]);
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    for (float i = -0.1; i < 0.0; i += 0.001) {
-        Stick[ELEVATOR] = i;
-        USBSerial.printf("ELEVETOR %6.3f\r\n", Stick[ELEVATOR]);
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
-    Stick[ELEVATOR] = 0.0;
-    USBSerial.printf("ELEVETOR %6.3f\r\n", Stick[ELEVATOR]);
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    for (float i = 0.0; i < 0.1; i += 0.001) {
-        Stick[ELEVATOR] = i;
-        USBSerial.printf("ELEVETOR %6.3f\r\n", Stick[ELEVATOR]);
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
-    Stick[ELEVATOR] = 0.1;
-    USBSerial.printf("ELEVETOR %6.3f\r\n", Stick[ELEVATOR]);
-    vTaskDelay(pdMS_TO_TICKS(1000));
-
-    for (float i = 0.1; i > 0.0; i -= 0.001) {
-        Stick[ELEVATOR] = i;
-        USBSerial.printf("ELEVETOR %6.3f\r\n", Stick[ELEVATOR]);
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
-    Stick[ELEVATOR] = 0.0;
-    USBSerial.printf("ELEVETOR %6.3f\r\n", Stick[ELEVATOR]);
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    data_task_running = false;
-    vTaskDelete(NULL);
+// 送信コールバック
+uint8_t esp_now_send_status;
+void on_esp_now_sent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+    esp_now_send_status = status;
 }
 
 void rc_init(void) {
     // Initialize Stick list
     for (uint8_t i = 0; i < 16; i++) Stick[i] = 0.0;
 
-    // BLEDevice::init("DRONE");
-    // BLEServer *pServer = BLEDevice::createServer();
-    // pServer->setCallbacks(new MyServerCallbacks());
-    // BLEService *pService = pServer->createService(SERVICE_UUID);
-    // pCharacteristic      = pService->createCharacteristic(
-    //     CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE |
-    //                              BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE);
-    // pCharacteristic->addDescriptor(new BLE2902());
-    // pCharacteristic->setCallbacks(new MyCallbacks());
-    // pService->start();
-    // BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-    // pAdvertising->addServiceUUID(SERVICE_UUID);
-    // pAdvertising->setScanResponse(true);
-    // pAdvertising->setMinPreferred(0x06);  // Helps with iPhone connections
-    // pAdvertising->setMaxPreferred(0x12);
-    // BLEDevice::startAdvertising();
-    // USBSerial.println("ESP-BLE Ready.");
-    xTaskCreatePinnedToCore(data_sender, "data_sender", 4096, NULL, 20, NULL, 0);
+    // ESP-NOW初期化
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+
+    WiFi.macAddress((uint8_t *)MyMacAddr);
+    USBSerial.printf("MAC ADDRESS: %02X:%02X:%02X:%02X:%02X:%02X\r\n", MyMacAddr[0], MyMacAddr[1], MyMacAddr[2],
+                     MyMacAddr[3], MyMacAddr[4], MyMacAddr[5]);
+
+    if (esp_now_init() == ESP_OK) {
+        USBSerial.println("ESPNow Init Success");
+    } else {
+        USBSerial.println("ESPNow Init Failed");
+        ESP.restart();
+    }
+
+    // MACアドレスブロードキャスト
+    uint8_t addr[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    memcpy(peerInfo.peer_addr, addr, 6);
+    peerInfo.channel = CHANNEL;
+    peerInfo.encrypt = false;
+    if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+        USBSerial.println("Failed to add peer");
+        return;
+    }
+    esp_wifi_set_channel(CHANNEL, WIFI_SECOND_CHAN_NONE);
+
+    // Send my MAC address
+    for (uint16_t i = 0; i < 50; i++) {
+        send_peer_info();
+        delay(50);
+        USBSerial.printf("%d\n", i);
+    }
+
+    // ESP-NOW再初期化
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    if (esp_now_init() == ESP_OK) {
+        USBSerial.println("ESPNow Init Success2");
+    } else {
+        USBSerial.println("ESPNow Init Failed2");
+        ESP.restart();
+    }
+
+    // ESP-NOWコールバック登録
+    esp_now_register_recv_cb(OnDataRecv);
+    USBSerial.println("ESP-NOW Ready.");
 }
 
 void send_peer_info(void) {
     uint8_t data[11];
     data[0] = CHANNEL;
     memcpy(&data[1], (uint8_t *)MyMacAddr, 6);
-    // memcpy(&data[1 + 6], (uint8_t *)peer_command, 4);
+    memcpy(&data[1 + 6], (uint8_t *)peer_command, 4);
+    esp_now_send(peerInfo.peer_addr, data, 11);
 }
 
 uint8_t telemetry_send(uint8_t *data, uint16_t datalen) {
@@ -356,13 +216,12 @@ uint8_t telemetry_send(uint8_t *data, uint16_t datalen) {
     esp_err_t result;
 
     if ((error_flag == 0) && (state == 0)) {
-        // result = esp_now_send(peerInfo.peer_addr, data, datalen);
-        cnt = 0;
-    } else {
+        result = esp_now_send(peerInfo.peer_addr, data, datalen);
+        cnt    = 0;
+    } else
         cnt++;
-    }
 
-    if (ble_send_status == 0) {
+    if (esp_now_send_status == 0) {
         error_flag = 0;
         // state = 0;
     } else {
@@ -385,12 +244,12 @@ void rc_end(void) {
 }
 
 uint8_t rc_isconnected(void) {
-    bool status = 1;
-    // Connect_flag++;
-    // if (Connect_flag < 40)
-    //     status = 1;
-    // else
-    //     status = 0;
+    bool status;
+    Connect_flag++;
+    if (Connect_flag < 40)
+        status = 1;
+    else
+        status = 0;
     // USBSerial.printf("%d \n\r", Connect_flag);
     return status;
 }
